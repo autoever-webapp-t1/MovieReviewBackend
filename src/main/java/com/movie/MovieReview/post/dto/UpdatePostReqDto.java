38b74cca276efc696bb5c8c7a4d24348b1e7209f
shining013 <nevertheless113@naver.com>
2024-11-10 22:01:13 +0900

    feat:포스트 updatedto 생성

---
+++ src/main/java/com/movie/MovieReview/post/dto/UpdatePostReqDto.java
@@ -0,0 +1,15 @@
+package com.movie.MovieReview.post.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdatePostReqDto {
+    private String title;
+    private String content;
+}
