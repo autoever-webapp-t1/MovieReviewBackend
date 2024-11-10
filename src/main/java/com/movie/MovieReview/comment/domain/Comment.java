8743a82ef8c42aecf983012e3067a5d3a947ef8f
shining013 <nevertheless113@naver.com>
2024-11-10 22:15:16 +0900

    feat:comment 엔티티 생성

---
+++ src/main/java/com/movie/MovieReview/comment/domain/Comment.java
@@ -0,0 +1,38 @@
+package com.movie.MovieReview.comment.domain;
+
+import com.movie.MovieReview.member.entity.MemberEntity;
+import com.movie.MovieReview.post.entitiy.Post;
+import com.movie.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long commentId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private MemberEntity writer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @Column(columnDefinition = "text", nullable = false)
+    private String content;
+
+    @Builder
+    public Comment(MemberEntity writer, Post post, String content) {
+        this.writer = writer;
+        this.post = post;
+        this.content = content;
+    }
+}
