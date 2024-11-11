151410d0359b641b5324d3f2ef12aa34a714fb48
hyeji <yhj061987@gmail.com>
2024-11-11 14:47:48 +0900

    save

--- src/main/java/com/movie/MovieReview/member/config/SecurityConfig.java
+++ src/main/java/com/movie/MovieReview/member/config/SecurityConfig.java
@@ -1,9 +1,55 @@
 package com.movie.MovieReview.member.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
+import static org.springframework.security.config.Customizer.withDefaults;
 
-@Configuration
+import com.movie.MovieReview.exception.ExceptionHandlerFilter;
+import com.movie.MovieReview.member.enums.MemberRole;
+import com.movie.MovieReview.member.filter.JwtFilter;
+import com.movie.MovieReview.member.service.JwtTokenService;
+import com.movie.MovieReview.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 @RequiredArgsConstructor
+@Configuration
 public class SecurityConfig {
+    private final JwtTokenService jwtTokenService;
+    private final MemberService memberService;
+
+    @Bean
+    public AuthenticationManager authenticationManager(final AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain configure(final HttpSecurity http) throws Exception {
+        return http.cors(withDefaults()).csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests(
+                        (authorize) -> authorize.requestMatchers("/**").permitAll().requestMatchers("/user/**")
+                                .hasAuthority(MemberRole.USER.getRole()).anyRequest().authenticated())
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer.disable()) // 로그인 폼 미사용
+                .httpBasic(httpSecurityHttpBasicConfigurer -> httpSecurityHttpBasicConfigurer.disable()) // http basic
+                // 미사용
+                .addFilterBefore(new JwtFilter(jwtTokenService, memberService),
+                        UsernamePasswordAuthenticationFilter.class) // JWT Filter 추가
+                .addFilterBefore(new ExceptionHandlerFilter(), JwtFilter.class) // Security Filter 에서 CustomException
+                // 사용하기 위해 추가
+                .build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        // 아래 url은 filter 에서 제외
+        return web -> web.ignoring().requestMatchers("/**");
+    }
 }
