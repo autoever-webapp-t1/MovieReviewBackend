77b94f7406cb2d885e7a16d8037fb27374e3a9fa
shining013 <nevertheless113@naver.com>
2024-11-11 16:09:00 +0900

    chore:주석처리

--- src/main/java/com/movie/MovieReview/comment/dao/CommentRepository.java
+++ src/main/java/com/movie/MovieReview/comment/dao/CommentRepository.java
@@ -1,12 +1,12 @@
-package com.movie.MovieReview.comment.dao;
-
-import com.movie.MovieReview.comment.domain.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-@Repository
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByPost_PostId(Long postId);
-}
+//package com.movie.MovieReview.comment.dao;
+//
+//import com.movie.MovieReview.comment.domain.Comment;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.List;
+//
+//@Repository
+//public interface CommentRepository extends JpaRepository<Comment, Long> {
+//    List<Comment> findByPost_PostId(Long postId);
+//}
