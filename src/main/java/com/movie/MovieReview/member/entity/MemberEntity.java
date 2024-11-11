cf33a4a29ef90b33d429b7845a0a07c8ad94b9df
shining013 <nevertheless113@naver.com>
2024-11-11 13:45:07 +0900

    fix: entity 생성 안되는 문제 처리

--- src/main/java/com/movie/MovieReview/member/entity/MemberEntity.java
+++ src/main/java/com/movie/MovieReview/member/entity/MemberEntity.java
@@ -1,4 +1,10 @@
 package com.movie.MovieReview.member.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class MemberEntity {
+    @Id
+    private Long memberId;
 }
