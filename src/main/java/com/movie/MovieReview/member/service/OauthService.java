b4efca1576cf567d8ed4598dab22964188dce654
hyeji <yhj061987@gmail.com>
2024-11-11 20:40:44 +0900

    test

--- src/main/java/com/movie/MovieReview/member/service/OauthService.java
+++ src/main/java/com/movie/MovieReview/member/service/OauthService.java
@@ -1,74 +1,87 @@
-package com.movie.MovieReview.member.service;
-
-import com.movie.MovieReview.exception.CustomException;
-import com.movie.MovieReview.exception.ErrorCode;
-import com.movie.MovieReview.member.dto.MemberDto;
-import com.movie.MovieReview.member.dto.TokenResponseDto;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.client.WebClient;
-
-import java.util.Map;
-
-@Service
-@RequiredArgsConstructor
-public class OauthService {
-    private final MemberService memberService;
-    private final KakaoOauthService kakaoOauthService;
-    private final JwtTokenService jwtTokenService;
-
-    //카카오 사용자 토큰 받아오기
-    public Map<String, Object> getKakaoToken(String code) {
-        WebClient webClient = WebClient.builder().baseUrl("https://kauth.kakao.com")
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE).build();
-
-        return webClient.post().uri("/oauth/token")
-                .body(BodyInserters.fromFormData("grant_type", "authorization_code")
-                        .with("client_id", "d8fabac493f22b719a1bc4f29b44c9d1")
-                        .with("redirect_uri", "http://localhost:8080/login/oauth/kakao").with("code", code))
-                .retrieve().bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {
-                }).block();
-
-    }
-
-    public String loginWithKakao(String accessToken, String refreshToken, HttpServletResponse response) {
-        MemberDto memberDto = kakaoOauthService.getUserProfileByToken(accessToken, refreshToken);
-
-        if (memberDto == null) {
-            throw new CustomException(ErrorCode.UNAUTHORIZED);
-        }
-        // 자체적인 JWT 토큰 생성
-        String JWTToken = jwtTokenService.createJWTToken(accessToken, refreshToken,
-                String.valueOf(memberDto.getMemberId()));
-
-        // TokenResponseDto 생성
-        TokenResponseDto tokenResponseDto = new TokenResponseDto();
-        tokenResponseDto.setAccessToken(accessToken);
-        tokenResponseDto.setRefreshToken(refreshToken);
-        return JWTToken; // TokenResponseDto 객체 반환
-    }
-
-    //카카오 사용자 로그아웃
-    public Map<String, Object> logout(String accessToken) {
-        WebClient webClient = WebClient.builder().baseUrl("https://kapi.kakao.com")
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken) // Access Token을 Authorization 헤더에 추가
-                .build();
-
-        // 로그아웃 요청
-        Map<String, Object> logoutResponse = webClient.post().uri("/v1/user/logout").retrieve()
-                .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {
-                }).block();
-        return logoutResponse; // 로그아웃 결과 응답
-    }
-
-    public MemberDto UserInfo(String accessToken, String refreshToken, HttpServletResponse response) {
-        MemberDto memberDto = kakaoOauthService.getUserProfileByToken(accessToken, refreshToken);
-        return memberDto;
-    }
-}
+//package com.movie.MovieReview.member.service;
+//
+//import com.movie.MovieReview.exception.CustomException;
+//import com.movie.MovieReview.exception.ErrorCode;
+//import com.movie.MovieReview.member.dto.MemberDto;
+//import com.movie.MovieReview.member.dto.TokenResponseDto;
+//import jakarta.servlet.http.HttpServletResponse;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.core.ParameterizedTypeReference;
+//import org.springframework.http.MediaType;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.reactive.function.BodyInserters;
+//import org.springframework.web.reactive.function.client.WebClient;
+//
+//import java.util.Map;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class OauthService {
+//    private final MemberService memberService;
+//    private final KakaoOauthService kakaoOauthService;
+//    private final JwtTokenService jwtTokenService;
+//
+//    //카카오 사용자 토큰 받아오기
+//    public Map<String, Object> getKakaoToken(String code) {
+//        WebClient webClient = WebClient.builder().baseUrl("https://kauth.kakao.com")
+//                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE).build();
+//
+//        return webClient.post().uri("/oauth/token")
+//                .body(BodyInserters.fromFormData("grant_type", "authorization_code")
+//                        .with("client_id", "d8fabac493f22b719a1bc4f29b44c9d1")
+//                        .with("redirect_uri", "http://localhost:8080/login/oauth/kakao").with("code", code))
+//                .retrieve().bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {
+//                }).block();
+//
+//    }
+//
+//    public String loginWithKakao(String accessToken, String refreshToken, HttpServletResponse response) {
+//        MemberDto memberDto = kakaoOauthService.getUserProfileByToken(accessToken, refreshToken);
+//
+//        if (memberDto == null) {
+//            throw new CustomException(ErrorCode.UNAUTHORIZED);
+//        }
+//        // 자체적인 JWT 토큰 생성
+//        String JWTToken = jwtTokenService.createJWTToken(accessToken, refreshToken,
+//                String.valueOf(memberDto.getMemberId()));
+//
+//        // TokenResponseDto 생성
+//        TokenResponseDto tokenResponseDto = new TokenResponseDto();
+//        tokenResponseDto.setAccessToken(accessToken);
+//        tokenResponseDto.setRefreshToken(refreshToken);
+//        return JWTToken; // TokenResponseDto 객체 반환
+//    }
+//
+//    //카카오 사용자 로그아웃
+//    public Map<String, Object> logout(String accessToken) {
+//        WebClient webClient = WebClient.builder().baseUrl("https://kapi.kakao.com")
+//                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+//                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken) // Access Token을 Authorization 헤더에 추가
+//                .build();
+//
+//        // 로그아웃 요청
+//        Map<String, Object> logoutResponse = webClient.post().uri("/v1/user/logout").retrieve()
+//                .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {
+//                }).block();
+//        return logoutResponse; // 로그아웃 결과 응답
+//    }
+//
+//    public MemberDto UserInfo(String accessToken, String refreshToken, HttpServletResponse response) {
+//        MemberDto memberDto = kakaoOauthService.getUserProfileByToken(accessToken, refreshToken);
+//        return memberDto;
+//    }
+//    // 리프레시 토큰으로 액세스토큰 새로 갱신
+//    public String refreshAccessToken(String refreshToken) {
+//        MemberDto memberDto = memberService.findByRefreshToken(refreshToken);
+//        if(memberDto == null) {
+//            throw new CustomException(ErrorCode.INVALID_REFRESH_TOKEN);
+//        }
+//
+//        if(!jwtTokenService.validateToken(refreshToken)) {
+//            throw new CustomException(ErrorCode.INVALID_REFRESH_TOKEN);
+//        }
+//
+//        return jwtTokenService.createAccessToken(memberDto.getMemberId().toString());
+//    }
+//}
--- src/main/java/com/movie/MovieReview/member/utils/SecurityUtil.java
+++ src/main/java/com/movie/MovieReview/member/utils/SecurityUtil.java
@@ -1,31 +1,31 @@
-package com.movie.MovieReview.member.utils;
-
-import com.movie.MovieReview.exception.CustomException;
-import com.movie.MovieReview.exception.ErrorCode;
-import com.movie.MovieReview.member.entity.UserPrincipal;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-
-public class SecurityUtil {
-    private SecurityUtil() {
-    }
-
-    public static Long getCurrentUserId() {
-
-        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        if (authentication == null) {
-            throw new CustomException(ErrorCode.UNAUTHORIZED);
-        }
-
-        Long userId;
-        System.out.println("#SecurityUtil_userId: ");
-        if (authentication.getPrincipal() instanceof UserPrincipal userPrincipal) {
-            userId = userPrincipal.getId();
-        } else {
-            throw new CustomException(ErrorCode.BAD_REQUEST);
-        }
-
-        return (Long) userId;
-    }
-}
+//package com.movie.MovieReview.member.utils;
+//
+//import com.movie.MovieReview.exception.CustomException;
+//import com.movie.MovieReview.exception.ErrorCode;
+//import com.movie.MovieReview.member.entity.UserPrincipal;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//
+//public class SecurityUtil {
+//    private SecurityUtil() {
+//    }
+//
+//    public static Long getCurrentUserId() {
+//
+//        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//
+//        if (authentication == null) {
+//            throw new CustomException(ErrorCode.UNAUTHORIZED);
+//        }
+//
+//        Long userId;
+//        System.out.println("#SecurityUtil_userId: ");
+//        if (authentication.getPrincipal() instanceof UserPrincipal userPrincipal) {
+//            userId = userPrincipal.getId();
+//        } else {
+//            throw new CustomException(ErrorCode.BAD_REQUEST);
+//        }
+//
+//        return (Long) userId;
+//    }
+//}
