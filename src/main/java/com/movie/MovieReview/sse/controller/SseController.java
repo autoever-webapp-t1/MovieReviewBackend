4714fad625d9bf19be90294cf36ae267ba6e08ab
shining013 <nevertheless113@naver.com>
2024-11-11 15:29:02 +0900

    feat:ssecontroller

--- src/main/java/com/movie/MovieReview/sse/controller/SseController.java
+++ src/main/java/com/movie/MovieReview/sse/controller/SseController.java
@@ -1,5 +1,6 @@
 package com.movie.MovieReview.sse.controller;
 
+import com.movie.MovieReview.sse.dto.MessageDto;
 import com.movie.MovieReview.sse.service.SseService;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -8,18 +9,22 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import java.io.IOException;
 
 @RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://ec2-43-201-114-161.ap-northeast-2.compute.amazonaws.com/")
 public class SseController {
-  private final SseService sseService;
+    private final SseService sseService;
 
-  public SseController(SseService sseService) {
-      this.sseService = sseService;
-  }
+    public SseController(SseService sseService) {
+        this.sseService = sseService;
+    }
 
-  @GetMapping(value = "/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-  public SseEmitter subscribe() {
-      return sseService.subscribe();
-  }
+    @GetMapping(value = "/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter subscribe() {
+        return sseService.subscribe();
+    }
 
-  @PostMapping("/notify")
-  }
+    @PostMapping("/notify")
+    public void notify(@RequestBody MessageDto messageDto) {
+        sseService.notify(messageDto);
+    }
 }
