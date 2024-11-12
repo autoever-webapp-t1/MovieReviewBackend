26035a77067abe3ac5ab340dc77db17ddca332fb
shining013 <nevertheless113@naver.com>
2024-11-12 14:00:49 +0900

    feat:post 테이블 생성

--- src/main/java/com/movie/MovieReview/post/entitiy/Post.java
+++ src/main/java/com/movie/MovieReview/post/entitiy/Post.java
@@ -1,13 +1,31 @@
 package com.movie.MovieReview.post.entitiy;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.movie.MovieReview.member.entity.MemberEntity;
+import com.movie.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Entity
-public class Post {
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long postId;
+
+    @ManyToOne()
+    @JoinColumn(name = "member_id")
+    private MemberEntity member;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private boolean isLiked;
 }
