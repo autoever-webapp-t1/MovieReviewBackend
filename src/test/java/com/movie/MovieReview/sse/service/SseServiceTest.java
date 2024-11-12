cced816cf763222031474cf90a290b10f3da8ab5
shining013 <nevertheless113@naver.com>
2024-11-12 12:14:43 +0900

    feat:sse 테스트 코드 작성

--- src/test/java/com/movie/MovieReview/sse/service/SseServiceTest.java
+++ src/test/java/com/movie/MovieReview/sse/service/SseServiceTest.java
@@ -8,6 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
+import java.util.concurrent.LinkedBlockingQueue;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -15,6 +18,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class SseServiceTest {
     private static final Logger log = LoggerFactory.getLogger(SseServiceTest.class);// 30분
     SseService sseService = new SseService();
+
     @Test
     void subscribe() {
         String emitter = sseService.subscribe().toString();
@@ -25,11 +29,19 @@ class SseServiceTest {
     void setMessage() {
         MessageDto messageDto = new MessageDto();
         messageDto.setMessage("test");
-        log.info("message: "+messageDto.getMessage());
         sseService.setMessage(messageDto);
     }
 
     @Test
     void alarm() {
+        MessageDto msd = new MessageDto();
+        msd.setMessage("test 2");
+        log.info("message: "+msd.getMessage());
+        SseEmitter emitter = new SseEmitter();
+        try {
+            emitter.send(SseEmitter.event().data(msd.getMessage()));
+        } catch (IOException e) {
+            log.info("error!");
+        }
     }
 }
