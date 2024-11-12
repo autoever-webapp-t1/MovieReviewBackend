1b029af596426ab7ca7cf89d8d59152822048797
shining013 <nevertheless113@naver.com>
2024-11-12 11:28:26 +0900

    feat:sse테스트코드 작성

--- src/test/java/com/movie/MovieReview/sse/service/SseServiceTest.java
+++ src/test/java/com/movie/MovieReview/sse/service/SseServiceTest.java
@@ -1,6 +1,8 @@
 package com.movie.MovieReview.sse.service;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -10,12 +12,13 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @Transactional
 class SseServiceTest {
-
+    private static final Logger log = LoggerFactory.getLogger(SseServiceTest.class);
+    SseEmitter se = new SseEmitter(60 * 1000L); // 30분
+    SseService sseService = new SseService();
     @Test
     void subscribe() {
-        SseEmitter se = new SseEmitter(60 * 1000L); // 30분
-
-
+        sseService.subscribe();
+        log.info("emitter's timeout " +sseService.emitter);
     }
 
     @Test
