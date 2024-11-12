5052509f1d4bee066f4f92440c10d2c220627463
shining013 <nevertheless113@naver.com>
2024-11-13 01:26:42 +0900

    chore
    # Conflicts:
    #	src/main/java/com/movie/global/entity/BaseTimeEntity.java

--- src/main/java/com/movie/global/entity/BaseTimeEntity.java
+++ src/main/java/com/movie/global/entity/BaseTimeEntity.java
@@ -1,6 +1,5 @@
 package com.movie.global.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
@@ -14,10 +13,7 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
     @CreatedDate
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")  // 원하는 형식으로 설정
-    private LocalDateTime createdDate;
-
+    private LocalDateTime createdAt;
     @LastModifiedDate
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")  // 원하는 형식으로 설정
-    private LocalDateTime modifiedDate;
+    private LocalDateTime updatedAt;
 }
