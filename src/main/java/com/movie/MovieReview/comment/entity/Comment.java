fdb6bf3b516996375a26e012d443f005349b200b
shining013 <nevertheless113@naver.com>
2024-11-12 17:21:06 +0900

    feat:comment 엔티티 수정

--- src/main/java/com/movie/MovieReview/comment/entity/Comment.java
+++ src/main/java/com/movie/MovieReview/comment/entity/Comment.java
@@ -4,8 +4,11 @@ import com.movie.MovieReview.member.entity.MemberEntity;
 import com.movie.MovieReview.post.entitiy.Post;
 import com.movie.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class Comment extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,4 +24,12 @@ public class Comment extends BaseTimeEntity {
 
     @Column(columnDefinition = "LONGTEXT", nullable = false)
     private String content;
+
+    @Builder
+    public Comment(MemberEntity writer, Post post, String content) {
+        this.writer = writer;
+        this.post = post;
+        this.content = content;
+    }
+
 }
