7a89dcce9212f6b29931a4394e30531107e5a18c
shining013 <nevertheless113@naver.com>
2024-11-13 01:11:24 +0900

    comment delete

--- src/main/java/com/movie/MovieReview/comment/service/CommentServiceImpl.java
+++ src/main/java/com/movie/MovieReview/comment/service/CommentServiceImpl.java
@@ -3,6 +3,7 @@ package com.movie.MovieReview.comment.service;
 import com.movie.MovieReview.comment.dao.CommentRepository;
 import com.movie.MovieReview.comment.dto.CommentReqDto;
 import com.movie.MovieReview.comment.dto.CommentResDto;
+import com.movie.MovieReview.comment.entity.Comment;
 
 import java.util.List;
 
@@ -11,11 +12,19 @@ public class CommentServiceImpl implements CommentService{
 
     @Override
     public void deleteComment(Long commentId) {
-        
+        if (commentRepository.findById(commentId)!=null){
+            commentRepository.delete(commentRepository.getReferenceById(commentId));
+        }
+        commentRepository.deleteById(commentId);
     }
 
     @Override
     public CommentResDto addComment(Long postId, CommentReqDto commentReqDto) {
+        // 각 포스트 하나에서 댓글을 달기
+        // 해당하는 포스트에서 댓글을 여러 개 단다. 프론트단에서 내용을 입력하면
+        // reqdto에 담아서 service 구현단을 호출하면 구현단에서 dto를 가지고 기능을 구현해
+        // 댓글 객체를 만들어서 reposiotry에 저장함
+
         return null;
     }
 
