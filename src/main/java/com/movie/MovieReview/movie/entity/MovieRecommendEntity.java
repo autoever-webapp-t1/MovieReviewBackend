d805a6fa05bd7e1034345f81d49cf6bed9fec839
N-jongsik <jongsik0625@naver.com>
2024-11-13 19:57:15 +0900

    feat: Save TopRatedId in DB

--- src/main/java/com/movie/MovieReview/movie/entity/MovieRecommendEntity.java
+++ src/main/java/com/movie/MovieReview/movie/entity/MovieRecommendEntity.java
@@ -2,6 +2,7 @@ package com.movie.MovieReview.movie.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,13 +10,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 @Table(name = "recommendations")
 public class MovieRecommendEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long recommendations_id;
+    private Long recommendationsId;
 
     @ManyToOne
     @JoinColumn(name = "id")
     MovieDetailEntity movieDetailEntity;
+
+    @Column(nullable = false)
+    private Long recommendationId;
 }
