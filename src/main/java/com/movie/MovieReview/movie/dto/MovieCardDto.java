250f6464d27a228e2be7facc2148a5b463d06f6d
N-jongsik <jongsik0625@naver.com>
2024-11-13 11:11:58 +0900

    feat: Save TopRated & Detail

--- src/main/java/com/movie/MovieReview/movie/dto/MovieCardDto.java
+++ src/main/java/com/movie/MovieReview/movie/dto/MovieCardDto.java
@@ -16,6 +16,6 @@ public class MovieCardDto {
     private String title; //movie제목
     private String overview; //movie 줄거리
     private String poster_path; //movie 포스터 url https://image.tmdb.org/t/p/w500/poster_path
-    private String release_date; //movie 개봉 날짜
+    private String release_date; //movie 개봉 날ㅉ
     private List<Integer> genre_ids; // movie 장르 ID들
 }
