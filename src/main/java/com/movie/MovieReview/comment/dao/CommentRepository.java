c0ae02618f1da5496e995c30fb7c48ec0ab48dc2
shining013 <nevertheless113@naver.com>
2024-11-13 19:37:41 +0900

    feat: 댓글 레파지토리 생성

--- src/main/java/com/movie/MovieReview/comment/dao/CommentRepository.java
+++ src/main/java/com/movie/MovieReview/comment/dao/CommentRepository.java
@@ -2,9 +2,21 @@ package com.movie.MovieReview.comment.dao;
 
 import com.movie.MovieReview.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    @Query("select c FROM Comment c WHERE c.post.postId = :postId")
     List<Comment> findByPost_PostId(Long postId);
+
+    Optional<Comment> findById(Long CommentId);
+
+    @Query("SELECT COUNT(c) from Comment c where c.post.postId=:id")
+    Integer countComment(@Param("id") Long postId);
+
 }
