ed4ff61ccaef08ca6a3022cbdffa76680e56f7fd
hyeji <yhj061987@gmail.com>
2024-11-13 12:04:51 +0900

    feat: review crud
    # Conflicts:
    #	src/main/java/com/movie/MovieReview/review/controller/ReviewController.java
    #	src/main/java/com/movie/MovieReview/review/service/ReviewService.java
    #	src/main/java/com/movie/MovieReview/review/service/ReviewServiceImpl.java
    #	src/main/java/com/movie/global/entity/BaseTimeEntity.java

--- src/main/java/com/movie/MovieReview/review/service/ReviewServiceImpl.java
+++ src/main/java/com/movie/MovieReview/review/service/ReviewServiceImpl.java
@@ -4,21 +4,17 @@ import com.movie.MovieReview.member.entity.MemberEntity;
 import com.movie.MovieReview.member.repository.MemberRepository;
 import com.movie.MovieReview.movie.entity.MovieDetailEntity;
 import com.movie.MovieReview.movie.repository.MovieRepository;
-import com.movie.MovieReview.review.dto.MyReviewsDto;
-import com.movie.MovieReview.review.dto.ReviewDetailDto;
+import com.movie.MovieReview.review.dto.ReviewDto;
 import com.movie.MovieReview.review.entity.ReviewEntity;
 import com.movie.MovieReview.review.repository.ReviewRepository;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-@Log4j2
 @Service
 public class ReviewServiceImpl implements ReviewService{
 
@@ -33,7 +29,7 @@ public class ReviewServiceImpl implements ReviewService{
 
     @Override
     @Transactional
-    public Long createReview(ReviewDetailDto dto) {
+    public Long createReview(ReviewDto dto) {
         Long memberId = dto.getMemberId();
         Long movieId = dto.getMovieId();
 
@@ -52,15 +48,14 @@ public class ReviewServiceImpl implements ReviewService{
         MovieDetailEntity foundMovie = movie.orElseThrow(() -> new IllegalArgumentException("Movie not found"));
 
         ReviewEntity reviewEntity = toEntity(dto, foundMember,foundMovie);
-
         ReviewEntity result = reviewRepository.save(reviewEntity);
-        log.info("result: " + result.getMember().getMemberId() );
+
         return result.getReviewId();
     }
 
     @Override
     @Transactional
-    public void modifyReview(ReviewDetailDto dto) {
+    public void modifyReview(ReviewDto dto) {
         Long memberId = dto.getMemberId();
         Long movieId = dto.getMovieId();
         Long reviewId = dto.getReviewId();
@@ -79,11 +74,7 @@ public class ReviewServiceImpl implements ReviewService{
 
         // 멤버와 영화가 존재하는지 확인
         MemberEntity foundMember = memberRepository.findById(memberId)
-                .orElseThrow(() -> {
-                    // 멤버가 존재하지 않으면 오류를 발생시킴
-                    System.out.println("Member not found for ID: " + memberId);  // 로그 추가
-                    return new IllegalArgumentException("Member not found");
-                });
+                .orElseThrow(() -> new IllegalArgumentException("Member not found"));
         MovieDetailEntity foundMovie = movieRepository.findById(movieId)
                 .orElseThrow(() -> new IllegalArgumentException("Movie not found"));
 
@@ -119,7 +110,7 @@ public class ReviewServiceImpl implements ReviewService{
     }
 
     @Override
-    public ReviewDetailDto getReview(Long reviewId) {
+    public ReviewDto getReview(Long reviewId) {
         ReviewEntity reviewEntity = reviewRepository.findById(reviewId)
                 .orElseThrow(() -> new IllegalArgumentException("Review not found"));
 
@@ -127,7 +118,7 @@ public class ReviewServiceImpl implements ReviewService{
     }
 
     @Override
-    public List<ReviewDetailDto> getAllReviews() {
+    public List<ReviewDto> getAllReviews() {
         List<ReviewEntity> reviews = reviewRepository.findAll();
         return reviews.stream().map(this::toDto).collect(Collectors.toList());
     }
@@ -141,41 +132,13 @@ public class ReviewServiceImpl implements ReviewService{
         reviewRepository.save(reviewEntity);
     }
 
-    @Override
-    public Map<String, Object> getAverageSkillsByMemberId(Long memberId) {
-        return reviewRepository.findAverageSkillsByMemberId(memberId);
-    }
-
-
-    @Override
-    @Transactional(readOnly = true)
-    public List<MyReviewsDto> getMemberReviews(Long memberId) {
-        // 해당 회원의 모든 리뷰 엔티티 가져오기
-        List<ReviewEntity> reviewEntities = reviewRepository.findAllReviewsByMemberId(memberId);
-
-        // ReviewEntity 리스트를 MyReviewsDto 리스트로 변환
-        return reviewEntities.stream()
-                .map(this::toMyPageDto)
-                .collect(Collectors.toList());
-    }
-
-    public ReviewDetailDto toDto(ReviewEntity reviewEntity){
-        double avgSkill = Math.round(
-                ((double)(reviewEntity.getActorSkill() + reviewEntity.getDirectorSkill() +
-                        reviewEntity.getSceneSkill() + reviewEntity.getMusicSkill() +
-                        reviewEntity.getStorySkill() + reviewEntity.getLineSkill()) / 12) * 100
-        ) / 100.0;
-
-        return ReviewDetailDto.builder()
+    public ReviewDto toDto(ReviewEntity reviewEntity){
+        return ReviewDto.builder()
                 .reviewId(reviewEntity.getReviewId())
-                .nickname(reviewEntity.getMember().getNickname())
-                .profile(reviewEntity.getMember().getProfile())
-                .memberId(reviewEntity.getMember().getMemberId())
+                .memberId(reviewEntity.getReviewId())
                 .movieId(reviewEntity.getMovie().getId())
-                .title(reviewEntity.getMovie().getTitle())
                 .content(reviewEntity.getContent())
                 .createdDate(reviewEntity.getCreatedDate())
-                .modifyDate(reviewEntity.getModifiedDate())
                 .totalHeart(reviewEntity.getTotalHeart())
                 .myHeart(reviewEntity.isMyHeart())
                 .actorSkill(reviewEntity.getActorSkill())
@@ -184,52 +147,24 @@ public class ReviewServiceImpl implements ReviewService{
                 .musicSkill(reviewEntity.getMusicSkill())
                 .storySkill(reviewEntity.getStorySkill())
                 .lineSkill(reviewEntity.getLineSkill())
-                .avgSkill(avgSkill)
+                .avgSkill((double)(reviewEntity.getActorSkill() + reviewEntity.getDirectorSkill() + reviewEntity.getSceneSkill() + reviewEntity.getMusicSkill() + reviewEntity.getStorySkill() + reviewEntity.getLineSkill()) / 12 )
                 .build();
     }
 
-    public MyReviewsDto toMyPageDto(ReviewEntity reviewEntity){
-
-        double avgSkill = Math.round(
-                ((double)(reviewEntity.getActorSkill() + reviewEntity.getDirectorSkill() +
-                        reviewEntity.getSceneSkill() + reviewEntity.getMusicSkill() +
-                        reviewEntity.getStorySkill() + reviewEntity.getLineSkill()) / 12) * 100
-        ) / 100.0;
-        return MyReviewsDto.builder()
-                .reviewId(reviewEntity.getReviewId())
-                .movieId(reviewEntity.getMovie().getId())
-                .title(reviewEntity.getMovie().getTitle())
-                .images(reviewEntity.getMovie().getImages())
-                .content(reviewEntity.getContent())
-                .createdDate(reviewEntity.getCreatedDate())
-                .modifyDate(reviewEntity.getModifiedDate())
-                .totalHeart(reviewEntity.getTotalHeart())
-                .myHeart(reviewEntity.isMyHeart())
-                .actorSkill(reviewEntity.getActorSkill())
-                .directorSkill(reviewEntity.getDirectorSkill())
-                .sceneSkill(reviewEntity.getSceneSkill())
-                .musicSkill(reviewEntity.getMusicSkill())
-                .storySkill(reviewEntity.getStorySkill())
-                .lineSkill(reviewEntity.getLineSkill())
-                .avgSkill(avgSkill)
-                .build();
-    }
-
-    public ReviewEntity toEntity(ReviewDetailDto reviewDetailDto, MemberEntity member, MovieDetailEntity movie){
-
+    public ReviewEntity toEntity(ReviewDto reviewDto, MemberEntity member, MovieDetailEntity movie){
         return ReviewEntity.builder()
-                .reviewId(reviewDetailDto.getReviewId())
+                .reviewId(reviewDto.getReviewId())
                 .movie(movie)
                 .member(member)
-                .content(reviewDetailDto.getContent())
-                .totalHeart(reviewDetailDto.getTotalHeart())
-                .myHeart(reviewDetailDto.isMyHeart())
-                .actorSkill(reviewDetailDto.getActorSkill())
-                .directorSkill(reviewDetailDto.getDirectorSkill())
-                .sceneSkill(reviewDetailDto.getSceneSkill())
-                .musicSkill(reviewDetailDto.getMusicSkill())
-                .storySkill(reviewDetailDto.getStorySkill())
-                .lineSkill(reviewDetailDto.getLineSkill())
+                .content(reviewDto.getContent())
+                .totalHeart(reviewDto.getTotalHeart())
+                .myHeart(reviewDto.isMyHeart())
+                .actorSkill(reviewDto.getActorSkill())
+                .directorSkill(reviewDto.getDirectorSkill())
+                .sceneSkill(reviewDto.getSceneSkill())
+                .musicSkill(reviewDto.getMusicSkill())
+                .storySkill(reviewDto.getStorySkill())
+                .lineSkill(reviewDto.getLineSkill())
                 .build();
     }
 }
