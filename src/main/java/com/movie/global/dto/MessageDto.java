3c751b244880ff9fd385a184ebefb7a337cab6c9
shining013 <nevertheless113@naver.com>
2024-11-13 19:35:56 +0900

    feat: ResponseEntity 메세지 dto 생성

---
+++ src/main/java/com/movie/global/dto/MessageDto.java
@@ -0,0 +1,13 @@
+package com.movie.global.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class MessageDto {
+    String msg;
+    public static MessageDto msg(String msg) {
+        return MessageDto.builder().msg(msg).build();
+    }
+}
