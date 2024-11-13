c9e7017da74d3d619aaf641e1e4803e1be1c41c2
shining013 <nevertheless113@naver.com>
2024-11-13 19:40:33 +0900

    feat: post에 댓글 관련 기능 구현

--- src/main/java/com/movie/MovieReview/post/entitiy/Post.java
+++ src/main/java/com/movie/MovieReview/post/entitiy/Post.java
@@ -5,9 +5,7 @@ import com.movie.MovieReview.comment.entity.Comment;
 import com.movie.MovieReview.member.entity.MemberEntity;
 import com.movie.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -36,10 +34,37 @@ public class Post extends BaseTimeEntity {
     private boolean isLiked;
     @Transient
     private Long likesCount;
+
+    @Setter
     private int commentCnt;
 
     @OrderBy("commentId desc")
     @JsonIgnoreProperties({"post"})
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
+
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public void addComment(Comment comment) {
+        this.comments.add(comment);
+        incrementCommentCount();
+    }
+
+    public void deleteComment(Comment comment) {
+        this.comments.remove(comment);
+        decrementCommentCount();
+    }
+
+    public void incrementCommentCount() {
+        this.commentCnt++;
+    }
+
+    public void decrementCommentCount() {
+        if (this.commentCnt > 0) {
+            this.commentCnt--;
+        }
+    }
 }
