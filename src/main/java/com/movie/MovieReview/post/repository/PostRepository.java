bdf1f5a5f244323e02f409ab342de6a0738423cc
shining013 <nevertheless113@naver.com>
2024-11-13 19:40:47 +0900

    chore: repository 어노테이션

--- src/main/java/com/movie/MovieReview/post/repository/PostRepository.java
+++ src/main/java/com/movie/MovieReview/post/repository/PostRepository.java
@@ -2,6 +2,9 @@ package com.movie.MovieReview.post.repository;
 
 import com.movie.MovieReview.post.entitiy.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post,Long> {
+
 }
