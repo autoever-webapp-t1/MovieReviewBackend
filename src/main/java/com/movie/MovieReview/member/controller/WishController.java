edc3b69b065fbf53e5140d5a7a00d0af92c3b2fc
hyeji <yhj061987@gmail.com>
2024-11-14 10:26:25 +0900

    for save

--- src/main/java/com/movie/MovieReview/member/controller/WishController.java
+++ src/main/java/com/movie/MovieReview/member/controller/WishController.java
@@ -1,7 +1,8 @@
 package com.movie.MovieReview.member.controller;
 
 import com.movie.MovieReview.member.entity.WishEntity;
-import com.movie.MovieReview.member.service.WishService;
+import com.movie.MovieReview.member.service.WishServiceImpl;
+import com.movie.MovieReview.movie.dto.MovieCardDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +14,7 @@ import java.util.List;
 public class WishController {
 
     @Autowired
-    private WishService wishService;
+    private WishServiceImpl wishServiceImpl;
 
     /**
      * 위시리스트에 영화를 추가하거나 삭제하는 API
@@ -25,7 +26,7 @@ public class WishController {
     public ResponseEntity<String> toggleWishlist(@RequestParam Long memberId, @RequestParam Long movieId) {
         try {
             // 위시리스트에 추가 또는 삭제 처리
-            wishService.addToWishlist(memberId, movieId);
+            wishServiceImpl.addToWishlist(memberId, movieId);
             return ResponseEntity.ok("위시리스트가 업데이트되었습니다.");
         } catch (Exception e) {
             return ResponseEntity.status(500).body("오류가 발생했습니다: " + e.getMessage());
@@ -37,9 +38,10 @@ public class WishController {
      * @return 회원의 위시리스트 항목 리스트
      */
     @GetMapping("/member/{memberId}")
-    public ResponseEntity<List<WishEntity>> getWishlistByMemberId(@PathVariable("memberId") Long memberId) {
-        List<WishEntity> wishlist = wishService.getWishlistByMemberId(memberId);
+    public ResponseEntity<List<MovieCardDto>> getWishlistByMemberId(@PathVariable("memberId") Long memberId) {
+        List<MovieCardDto> wishlist = wishServiceImpl.getWishlistByMemberId(memberId);
         return ResponseEntity.ok(wishlist);
     }
+
 
 }
