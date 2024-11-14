3898c900e99b455b21f8b911a2e83d27fcb2607e
shining013 <nevertheless113@naver.com>
2024-11-14 10:58:38 +0900

    feat:post dto 생성

---
+++ src/main/java/com/movie/MovieReview/post/dto/PostDtoInterface.java
@@ -0,0 +1,13 @@
+package com.movie.MovieReview.post.dto;
+
+import java.time.LocalDateTime;
+
+public interface PostDtoInterface {
+    Long getPostId();
+    String title();
+    String content();
+    boolean isLiked();
+    Integer likesCount();
+    LocalDateTime createdDate();
+    LocalDateTime modifiedDate();
+}
