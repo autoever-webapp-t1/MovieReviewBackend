d7a5496abb5c325c71304d76ac8db6b96832fd7e
shining013 <nevertheless113@naver.com>
2024-11-15 11:47:46 +0900

    feat: 쿼리문 작성

--- src/main/java/com/movie/MovieReview/post/repository/PostRepository.java
+++ src/main/java/com/movie/MovieReview/post/repository/PostRepository.java
@@ -2,6 +2,9 @@ package com.movie.MovieReview.post.repository;
 
 import com.movie.MovieReview.post.entitiy.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +12,12 @@ import java.util.Optional;
 @Repository
 public interface PostRepository extends JpaRepository<Post,Long> {
     Optional<Post> findById(Long postId);
+
+    @Modifying
+    @Query("UPDATE Post p SET p.likesCount = p.likesCount + 1 WHERE p.id = :postId")
+    void incrementLikeCount(@Param("postId") Long postId);
+
+    @Modifying
+    @Query("UPDATE Post p SET p.likesCount = p.likesCount - 1 WHERE p.id = :postId")
+    void decrementLikeCount(@Param("postId") Long postId);
 }
