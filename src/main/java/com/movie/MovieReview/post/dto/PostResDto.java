d520a84d4f1c29585ed90f8b4c9fe1941c593dcd
shining013 <nevertheless113@naver.com>
2024-11-15 17:32:46 +0900

    feat:post dto

---
+++ src/main/java/com/movie/MovieReview/post/dto/PostResDto.java
@@ -0,0 +1,71 @@
+package com.movie.MovieReview.post.dto;
+
+import com.movie.MovieReview.post.entitiy.Post;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class PostResDto implements PostDtoInterface{
+    private Long postId;
+    private String nickname;
+    private String title;
+    private String content;
+    private Integer commentCnt;
+    private boolean isLiked;
+    private Integer likesCount;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+
+    public static PostResDto entityToResDto(Post post) {
+        return PostResDto.builder()
+                .postId(post.getPostId())
+                .nickname(post.getWriter().getNickname())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .commentCnt(post.getCommentCnt())
+                .isLiked(post.isLiked())
+                .likesCount(post.getLikesCount())
+                .createdDate(post.getCreatedDate())
+                .modifiedDate(post.getModifiedDate())
+                .build();
+    }
+
+    public String getNickname() {return nickname;}
+    public boolean getIsLiked() {return isLiked;}
+    @Override
+    public Long getPostId() {
+        return postId;
+    }
+
+    @Override
+    public String title() {
+        return title;
+    }
+
+    @Override
+    public String content() {
+        return content;
+    }
+
+    @Override
+    public boolean isLiked() {
+        return isLiked;
+    }
+
+    @Override
+    public Integer likesCount() {
+        return likesCount;
+    }
+
+    @Override
+    public LocalDateTime createdDate() {
+        return createdDate;
+    }
+
+    @Override
+    public LocalDateTime modifiedDate() {
+        return modifiedDate;
+    }
+}
