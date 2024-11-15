9d2e8cbf0b8f0eb02ab1ec9b252921533e4f1a79
N-jongsik <jongsik0625@naver.com>
2024-11-15 22:03:41 +0900

    feat: search pagenation

--- src/main/java/com/movie/MovieReview/movie/repository/MovieRepository.java
+++ src/main/java/com/movie/MovieReview/movie/repository/MovieRepository.java
@@ -1,7 +1,8 @@
 package com.movie.MovieReview.movie.repository;
 
-import com.movie.MovieReview.movie.dto.MovieCardDto;
 import com.movie.MovieReview.movie.entity.MovieDetailEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,4 +12,7 @@ public interface MovieRepository extends JpaRepository<MovieDetailEntity,Long> {
     Optional<MovieDetailEntity> findByTitle(String title);
 
     List<MovieDetailEntity> findByTitleContaining(String query);
+
+    Page<MovieDetailEntity> findByTitleContaining(String title, Pageable pageable);
+
 }
