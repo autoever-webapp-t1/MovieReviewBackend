f4326fc5e1086a07ad99b9a44e3910f6ec91c736
shining013 <nevertheless113@naver.com>
2024-11-15 17:32:18 +0900

    feat:comment 서비스 구현

---
+++ src/main/java/com/movie/MovieReview/comment/service/CommentService.java
@@ -0,0 +1,12 @@
+package com.movie.MovieReview.comment.service;
+import com.movie.MovieReview.comment.dto.CommentReqDto;
+import com.movie.MovieReview.comment.dto.CommentResDto;
+import java.util.List;
+public interface CommentService {
+    void deleteComment(Long commentId);
+    CommentResDto addComment(Long postId, CommentReqDto commentReqDto);
+    CommentResDto updateComment(Long commentId, CommentResDto commentResDto);
+    List<CommentResDto> findCommentByPostId(Long postId);
+    CommentResDto findOne(Long commentId);
+    Integer countComment(Long postId);
+}
