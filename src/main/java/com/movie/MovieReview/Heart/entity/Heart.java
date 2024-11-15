e1eabf0634efa6b2ca13bc831e2ebe37fd346028
shining013 <nevertheless113@naver.com>
2024-11-15 18:20:06 +0900

    feat: 좋아요

--- src/main/java/com/movie/MovieReview/Heart/entity/Heart.java
+++ src/main/java/com/movie/MovieReview/Heart/entity/Heart.java
@@ -4,6 +4,7 @@ import com.movie.MovieReview.member.entity.MemberEntity;
 import com.movie.MovieReview.post.entitiy.Post;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 @Entity
@@ -15,10 +16,18 @@ public class Heart {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "heart_id")
     private Long id;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private MemberEntity member;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id")
     private Post post;
+
+    @Builder
+    public Heart(MemberEntity member, Post post) {
+        this.member = member;
+        this.post = post;
+    }
 }
