522bab965cab9432011a3bced45f4dc10b405ea4
shining013 <nevertheless113@naver.com>
2024-11-17 00:40:00 +0900

    feat: sse 구현 수정

--- src/main/java/com/movie/MovieReview/sse/service/SseService.java
+++ src/main/java/com/movie/MovieReview/sse/service/SseService.java
@@ -1,5 +1,8 @@
 package com.movie.MovieReview.sse.service;
 
+import com.movie.MovieReview.member.entity.MemberEntity;
+import com.movie.MovieReview.member.entity.UserPrincipal;
+import com.movie.MovieReview.member.repository.MemberRepository;
 import com.movie.MovieReview.sse.dto.MessageDto;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -9,7 +12,9 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -17,12 +22,21 @@ import java.util.concurrent.LinkedBlockingQueue;
 @Service
 @Log4j2
 public class SseService {
-    private final Set<SseEmitter> emitters = new CopyOnWriteArraySet<>();
+    private final Map<Long, SseEmitter> emitters = new ConcurrentHashMap<>();
+    private UserPrincipal userPrincipal;
+    private MemberRepository memberRepository;
+
+    private MemberEntity getLoginMember() {
+        String loginMemberEmail = userPrincipal.getEmail();
+        return memberRepository.findByEmail(loginMemberEmail)
+                .orElseThrow(()->new RuntimeException("member not found"));
+    }
 
     private final LinkedBlockingQueue<MessageDto> messageQueue = new LinkedBlockingQueue<>();
     private SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
     public SseEmitter subscribe() {
-
+        Long memberId = getLoginMember().getMemberId();
+        emitters.put(memberId, emitter);
         try {
             emitter.send(SseEmitter.event()
                     .name("connect")
@@ -30,7 +44,7 @@ public class SseService {
         } catch (IOException e) {
             emitter.completeWithError(e);
         }
-        emitters.add(emitter);
+
         emitter.onCompletion(()->emitters.remove(emitter));
         emitter.onTimeout(()->emitters.remove(emitter));
         emitter.onError((e)->emitters.remove(emitter));
@@ -44,19 +58,23 @@ public class SseService {
 //        @Scheduled(cron = "0 0 21 ? * MON") // At 09:00 PM, 매주 목요일 실행
     @Scheduled(cron = "0 * * * * *") // 매분 0초에 실행(테스트용)
     public void alarm() {
+        Long memberId = userPrincipal.getId();
         MessageDto messageDto = messageQueue.poll();
         if (messageDto != null) {
             String message = messageDto.getMessage();
-            log.info("message: "+ message);
-            Iterator<SseEmitter> iterator = emitters.iterator();
-            while (iterator.hasNext()) {
-                SseEmitter emitter = iterator.next();
+            log.info("message: " + message);
+            emitter = emitters.get(memberId);
+            if (emitter != null) {
                 try {
                     emitter.send(SseEmitter.event().data(message));
                 } catch (IOException e) {
-                    iterator.remove();
+                    emitters.remove(memberId);
                 }
             }
+
         }
     }
+
 }
+
+
