6f130454c588248e9149f5cb85dc67c9f8095fee
N-jongsik <jongsik0625@naver.com>
2024-11-16 22:25:22 +0900

    feat: movie recommendations

--- src/main/java/com/movie/MovieReview/movie/controller/MovieController.java
+++ src/main/java/com/movie/MovieReview/movie/controller/MovieController.java
@@ -11,12 +11,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -141,6 +139,12 @@ public class MovieController {
         PageResponseDto<MovieCardDto> response = movieService.getAllMovieByKeyword(keyword, pageRequestDto);
 
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{memberId}/recommendations")
+    public ResponseEntity<List<MovieCardDto>> getMemberRecommendByMemberId(@PathVariable("memberId") Long memberId) {
+        List<MovieCardDto> movieCards = movieService.getMovieMemberRecommendations(memberId);
+        return ResponseEntity.ok(movieCards);
     }
 }
 
