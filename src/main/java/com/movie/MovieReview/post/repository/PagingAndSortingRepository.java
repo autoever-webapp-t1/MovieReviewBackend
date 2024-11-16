252d93e6384783543d7d36f5769dfb5fae3d2400
shining013 <nevertheless113@naver.com>
2024-11-16 23:28:48 +0900

    feat: querydsl 인터페이스 적용

--- src/main/java/com/movie/MovieReview/post/repository/PagingAndSortingRepository.java
+++ src/main/java/com/movie/MovieReview/post/repository/PagingAndSortingRepository.java
@@ -4,9 +4,10 @@ import com.movie.MovieReview.post.entitiy.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
-public interface PagingAndSortingRepository extends JpaRepository<Post, Long> {
+public interface PagingAndSortingRepository extends JpaRepository<Post, Long>, QuerydslPredicateExecutor<Post> {
     Page<Post> findAll(Pageable pageable);
 }
