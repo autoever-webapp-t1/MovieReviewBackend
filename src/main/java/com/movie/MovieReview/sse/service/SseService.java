40c7c62d87da092948922c38a61bec23cb6aa1e9
shining013 <nevertheless113@naver.com>
2024-11-17 00:44:58 +0900

    feat: sse 구현 수정

--- src/main/java/com/movie/MovieReview/sse/service/SseService.java
+++ src/main/java/com/movie/MovieReview/sse/service/SseService.java
@@ -7,15 +7,14 @@ import com.movie.MovieReview.sse.dto.MessageDto;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.LinkedBlockingQueue;
 
 
@@ -58,7 +57,13 @@ public class SseService {
 //        @Scheduled(cron = "0 0 21 ? * MON") // At 09:00 PM, 매주 목요일 실행
     @Scheduled(cron = "0 * * * * *") // 매분 0초에 실행(테스트용)
     public void alarm() {
-        Long memberId = userPrincipal.getId();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication.getPrincipal() instanceof UserPrincipal)) {
+            log.warn("사용자 인증정보가 없습니다");
+            return;
+        }
+        UserPrincipal userPrincipal1 = (UserPrincipal) authentication.getPrincipal();
+        Long memberId = userPrincipal1.getId();
         MessageDto messageDto = messageQueue.poll();
         if (messageDto != null) {
             String message = messageDto.getMessage();
@@ -70,8 +75,9 @@ public class SseService {
                 } catch (IOException e) {
                     emitters.remove(memberId);
                 }
+            } else  {
+                log.warn("이 사용자에 대한 emitter가 존재하지 않습니다. userId: {}", memberId);
             }
-
         }
     }
 
