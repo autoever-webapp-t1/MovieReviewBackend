8be9e4b23b289f70d928b0d4aff8c8829005d984
N-jongsik <jongsik0625@naver.com>
2024-11-17 18:48:56 +0900

    feat: add recommendations api

--- src/main/java/com/movie/MovieReview/movie/service/MovieService.java
+++ src/main/java/com/movie/MovieReview/movie/service/MovieService.java
@@ -16,6 +16,7 @@ public interface MovieService {
     public List<MovieCardDto> getNowPlayingMovies(Long memberId) throws Exception; //NowPlaying 100개 영화정보 가져오기
     public List<MovieCardDto> getUpComingMovies(Long memberId) throws Exception; //UpComing 100개 영화정보 가져오기
     public List<MovieCardDto> getPopularMovies(Long memberId) throws Exception;
+    public List<MovieCardDto> getRecommendMovies(Long movieId, Long memberId) throws Exception;
 
     public MovieDetailsDto getMovieDetails(Long id) throws Exception; //영화상세정보 가져오기
     public List<Long> SaveTopRatedId() throws Exception; //TopRated ID들 저장
