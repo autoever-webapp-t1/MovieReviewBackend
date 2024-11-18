8a4bf4062732fa5f03a916a1c1200a99da7f9c8d
hyeji <yhj061987@gmail.com>
2024-11-19 01:32:09 +0900

    feat: CORS 추가

--- src/main/java/com/movie/MovieReview/s3/controller/UploadController.java
+++ src/main/java/com/movie/MovieReview/s3/controller/UploadController.java
@@ -1,22 +1,22 @@
-//package com.movie.MovieReview.s3.controller;
-//
-//import com.movie.MovieReview.s3.service.S3Upload;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
-//import org.springframework.web.multipart.MultipartFile;
-//import java.io.IOException;
-//@RequiredArgsConstructor
-//@RestController
-//public class UploadController{
-//    private final S3Upload s3Upload;
-//    @PostMapping("/api/upload")
-//    public ResponseEntity<String> uploadFile(@RequestParam("images") MultipartFile multipartFile) throws IOException {
-//        long fileSize = multipartFile.getSize();
-//        String uploadedFileUrl = s3Upload.upload(multipartFile.getInputStream(), multipartFile.getOriginalFilename(), fileSize);
-//        return new ResponseEntity<>(uploadedFileUrl, HttpStatus.CREATED);
-//    }
-//}
+package com.movie.MovieReview.s3.controller;
+
+import com.movie.MovieReview.s3.service.S3Upload;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+@RequiredArgsConstructor
+@RestController
+public class UploadController{
+    private final S3Upload s3Upload;
+    @PostMapping("/api/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("images") MultipartFile multipartFile) throws IOException {
+        long fileSize = multipartFile.getSize();
+        String uploadedFileUrl = s3Upload.upload(multipartFile.getInputStream(), multipartFile.getOriginalFilename(), fileSize);
+        return new ResponseEntity<>(uploadedFileUrl, HttpStatus.CREATED);
+    }
+}
