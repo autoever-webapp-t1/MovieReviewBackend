fbf2ecc29c58cccb40c776033b9c56ed5b7068f9
shining013 <nevertheless113@naver.com>
2024-11-19 15:25:22 +0900

    fix: messageDto 수정

--- src/main/java/com/movie/MovieReview/sse/dto/MessageDto.java
+++ src/main/java/com/movie/MovieReview/sse/dto/MessageDto.java
@@ -3,8 +3,10 @@ package com.movie.MovieReview.sse.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Setter
 @Getter
